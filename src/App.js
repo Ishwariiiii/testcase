f8b6294be37541c823d90767d8a003fb0d8499ce
Ishwariiiii <ishwarikanathe@gmail.com>
2024-11-06 06:42:49 -0800

    06-11-2024

--- src/App.js
+++ src/App.js
@@ -3,28 +3,80 @@ import Test from './Test'
 
 const App = () => {
   const [data, setData] = useState("")
+  const [inputData, setInputData] = useState("")
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
-    <div>
-      <h1>testing by text</h1>
+    <div className='container'>
+      {/* <h1>testing by text</h1> */}
       <p>Hello This Is Test Case </p>
+     
+
       <hr />
-      <h1>testing bu image</h1>
+
+      {/* <h1>testing by image</h1> */}
       <img title='nature image' src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fdiscover%2Ffree-nature-images&psig=AOvVaw0KegETW51IunqdiJZzQdqb&ust=1730896000487000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIjBu6ysxYkDFQAAAAAdAAAAABAE" />
       <br />
       <hr />
-      <h1>testing bu input field</h1>
 
-      <input type='text' placeholder='Enter Username' name='data' value={data} onChange={(e) => setData(e.target.value)}/>
+      {/* <h1>testing by input boxes</h1> */}
+      <input type='text' placeholder='Enter Username' name='data' value={data} id='userId' onChange={(e) => setData(e.target.value)} />
 
-      <h1>testing by button</h1>
+      {/* <h1> describe</h1> */}
+      <input type='text' placeholder=' Username' />
+      <input type='text' placeholder=' Username' />
+      <input type='text' placeholder=' Username' />
+
+
+      {/* <h1>onchange event testing</h1> */}
+      <input type='text' placeholder='userUser' name='inputData' value={data} id='userId' onChange={(e) => setInputData(e.target.value)} />
+
+
+      {/* <h1>testing by button</h1> */}
       <button>Save</button>
+      <button>click</button>
+      <div role='dummy'>Dummy text</div>
       <hr />
 
-      <label htmlFor='user-name' >Username</label>
-      <input type='text' id='user-name' />
 
+      <h1>input type test by labels</h1>
+      <label htmlFor='user-name1' >Name</label>
+      <input type='text' id='user-name1' placeholder='name' />
       <br />
       <br />
+
+      <label htmlFor='user-name2' >Email</label>
+      <input type='text' id='user-name2' placeholder='name' />
+      <br />
+      <br />
+
+      <label htmlFor='user-name3' >Address</label>
+      <input type='text' id='user-name3' placeholder='name' />
+
+      <hr />
+
+      <h1>checkboxes</h1>
+      <label htmlFor='skill1' >Skills</label>
+      <input type='checkbox' id='skill1' defaultChecked={true} checked />
+
+      <label htmlFor='skill2' >Skills</label>
+      <input type='checkbox' id='skill2' defaultChecked={true} checked />
+      <hr />
+
+      <h1>controlld components (Form)</h1>
+
+      <form>
+        <input type='text' placeholder='Enter name' value={name} onChange={(e) => setName(e.target.value)} />
+        <br></br>
+        <input type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <br></br>
+        <input type='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <br></br>
+        <button>save</button>
+      </form>
+      <br />
+
 
       <Test />
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -2,35 +2,71 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App"
 
 test("test app component", () => {
-  render(<App/>)
+  render(<App />)
+  //test text
   const text = screen.getByText(/Hello This Is Test Case/i)
+  //test image
   const title = screen.getByTitle(/nature image/i)
   expect(text).toBeInTheDocument()
   expect(title).toBeInTheDocument()
 })
 
-test("on change event testing", () => {
-  render(<App/>)
+test("testing input box", () => {
+  render(<App />)
   const inputCheck = screen.getByPlaceholderText("Enter Username")
-  fireEvent.change(inputCheck, {target: {value:'abc'}});
+  fireEvent.change(inputCheck, { target: { value: 'abc' } });
   expect(inputCheck.value).toBe("abc");
-  })
+  expect(inputCheck).toHaveAttribute("id", "userId")
+})
 
-describe("ui test", () => {
+describe("ui test input fields", () => {
   test("test 1", () => {
-    render(<App/>)
+    render(<App />)
     const input = screen.getAllByRole("textbox")
-    for(let i=0;i<input.length;i++){
+    for (let i = 0; i < input.length; i++) {
       expect(input[i]).toBeInTheDocument();
     }
-    
+
   })
 })
 
-test("getByLabelText",()=>{
-  render(<App/>)
-  const input=screen.getByLabelText("Username")
-  expect (input).toBeInTheDocument()
+
+test("onchange event testing", () => {
+  render(<App />)
+  const input = screen.getByPlaceholderText("userUser")
+  fireEvent.change(input, { target: { value: "" } })
+  expect(input.value).toBe("");
 })
 
+
+
+
+test("getByRole button test", () => {
+  render(<App />)
+  const btn1 = screen.getByRole("button", { name: "save" })
+  const btn2 = screen.getByRole("button", { name: "click" })
+
+  const dv1 = screen.getByRole("dummy")
+  expect(btn1).toBeInTheDocument()
+  expect(btn2).toBeInTheDocument()
+  expect(dv1).toBeInTheDocument()
+})
+
+test("input test case", () => {
+  render(<App />)
+  const input = screen.getAllByPlaceholderText("name")
+  // expect (input[1]).toBeInTheDocument()
+  for (let i = 0; i < input.length; i++) {
+    expect(input[i]).toBeInTheDocument()
+  }
+})
+
+test("checkbox test case", () => {
+  render(<App />)
+  const checkbox = screen.getAllByLabelText("Skills")
+  for (let i = 0; i < checkbox.length; i++) {
+    expect(checkbox[i]).toBeInTheDocument();
+    expect(checkbox[i]).toBeChecked();
+  }
+})
 
